374730457af94ccd450702f4338b928e84913f93
Angel Almanza <almanza.angel245@gmail.com>
2023-02-27 16:03:26 -0700

    Entrada de datos activada, pendiente compilar y archivo README

--- main.cpp
+++ main.cpp
@@ -1,20 +1,60 @@
 #include <iostream>
+#include <fstream>
 #include "sort.h"
 
 using namespace std;
 
-void imprimir(int[], int n);
+string imprimir(int[], int n);
+void escribir(string);
+int *llenar(int[]);
 
 int main() {
-  int numeros[9] = {50, 3, 5, 10, 9, 11, 1, 25, 19};
+  string txt = "Arreglo desordenado\n";
+  // int numeros[] = {50, 3, 5, 10, 9, 11, 1, 25, 19};
+  int numerosA[9];
+
+  cout << "Introduce 9 numeros para el arreglo\n";
+  int *numeros = llenar(numerosA);
+
+  txt += imprimir(numeros, 9);
   int *ptr = shell(numeros, 9);
-  imprimir(ptr, 9);
+  txt += ("Arreglo ordenado\n" + imprimir(ptr, 9));
+
+  escribir(txt);
+
   return 0;
 }
 
-void imprimir(int arreglo[], int n) {
+string imprimir(int arreglo[], int n) {
+  string txt = "";
+
   for (int i = 0; i < n; i++) {
-    cout << "[" << arreglo[i] << "] ";
+    txt += "[" + to_string(arreglo[i]) + "] ";
   }
-    cout << "\n";
+
+  txt += "\n";
+
+  return txt;
+}
+
+void escribir(string txt) {
+  ofstream sort;
+  sort.open("./sort.txt", ios::out);
+
+  if (sort.fail()) {
+    cout << "No se pudo abrir el archivo" << endl;
+  }
+
+  sort << txt;
+  sort.close();
+}
+
+int *llenar(int arreglo[]) {
+  int x;
+  for (int i = 0; i < 9; i++) {
+    cout << "Dame el siguiente valor" << endl;
+    cin >> x;
+    arreglo[i] = x;
+  }
+  return arreglo;
 }
